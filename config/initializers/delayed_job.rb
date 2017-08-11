d886c1289662623148881fe6c50d49c6b64df29a
Rene Weteling <rene@weteling.com>
2017-08-11 17:10:58 +0200

    Disabled clocl

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,12 +1,9 @@
 Delayed::Worker.destroy_failed_jobs = false
-Delayed::Worker.sleep_delay = 5
+Delayed::Worker.sleep_delay = 30
 Delayed::Worker.max_attempts = 4
-Delayed::Worker.max_run_time = 4.hours
+Delayed::Worker.max_run_time = 10.minutes
 Delayed::Worker.read_ahead = 10
 Delayed::Worker.default_queue_name = 'default'
 Delayed::Worker.delay_jobs = !Rails.env.test?
 Delayed::Worker.raise_signal_exceptions = :term
 Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
-
-# enable only on productino
-Delayed::Worker.delay_jobs = Rails.env.production?
