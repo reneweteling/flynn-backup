28cec72af74fcf7896331ad167327abf094fce27
Rene Weteling <rene@weteling.com>
2017-08-11 16:42:56 +0200

    Clcik

--- config/clock.rb
+++ config/clock.rb
@@ -1,4 +1,4 @@
-require 'config/boot'
-require 'config/environment'
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'config', 'boot'))
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'config', 'environment'))
 
 Clockwork.every(1.minute, "BackupJob") { system "bundle exec bin/rails runner 'BackupJob::perform_later'" }
