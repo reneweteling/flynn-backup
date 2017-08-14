9cc3229693de31c74e16f4d9b51f8fc7b79d5bdd
Rene Weteling <rene@weteling.com>
2017-08-14 16:32:50 +0200

    Initializer

--- config/initializers/flynn.rb
+++ config/initializers/flynn.rb
@@ -1,11 +1,11 @@
 # this will add the .flynnrc file that is used by the flynn cli
-return unless Rails.env.production?
-
-flynnrc = File.join(ENV['HOME'], '.flynnrc')
-return if File.file?(flynnrc)
-
-html = File.open(Rails.root.join('vendor', 'flynnrc.erb')).read
-template = ERB.new(html)
-File.open(flynnrc, 'w') do |f|
-  f.puts template.result
+if Rails.env.production?
+  flynnrc = File.join(ENV['HOME'], '.flynnrc')
+  if File.file?(flynnrc)
+    html = File.open(Rails.root.join('vendor', 'flynnrc.erb')).read
+    template = ERB.new(html)
+    File.open(flynnrc, 'w') do |f|
+      f.puts template.result
+    end
+  end
 end
