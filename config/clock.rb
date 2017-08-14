418fb4e8311eae452e802ef425b97c99f306fec7
Rene Weteling <rene@weteling.com>
2017-08-14 09:49:50 +0200

    s

--- config/clock.rb
+++ config/clock.rb
@@ -1,4 +1,4 @@
 require './config/boot'
 require './config/environment'
 
-Clockwork.every(1.minute, "BackupJob") { BackupJob.perform_later }
+Clockwork.every(1.minute, "BackupJob") { BackupJob.delay(run_at: 10.seconds.from_now) }
