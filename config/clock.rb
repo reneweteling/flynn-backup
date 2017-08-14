79a48246646f3d685e437e54ec0e1113dcd1721a
Rene Weteling <rene@weteling.com>
2017-08-14 08:29:17 +0200

    Clock

--- config/clock.rb
+++ config/clock.rb
@@ -1,5 +1,4 @@
-require 'clockwork'
-require './config/boot'
-require './config/environment'
+# require './config/boot'
+# require './config/environment'
 
-Clockwork.every(1.minute, "BackupJob") { system "bundle exec bin/rails runner 'BackupJob::perform_later'" }
+Clockwork.every(1.minute, "BackupJob") { system "bundle exec bin/rails runner 'BackupJob.delay(run_at: 10.seconds.from_now)'" }
