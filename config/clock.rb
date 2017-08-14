b66f947efadec801e68e0c8fcbf28cd2ab8ed9eb
Rene Weteling <rene@weteling.com>
2017-08-14 08:20:39 +0200

    Clock over dj

--- config/clock.rb
+++ config/clock.rb
@@ -1,4 +1,5 @@
-require File.expand_path(File.join(File.dirname(__FILE__), '..', 'config', 'boot'))
-require File.expand_path(File.join(File.dirname(__FILE__), '..', 'config', 'environment'))
+require 'clockwork'
+require './config/boot'
+require './config/environment'
 
-# Clockwork.every(1.minute, "BackupJob") { system "bundle exec bin/rails runner 'BackupJob::perform_later'" }
+Clockwork.every(1.minute, "BackupJob") { system "bundle exec bin/rails runner 'BackupJob::perform_later'" }
