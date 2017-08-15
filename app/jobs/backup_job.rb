19ae40563c3ef8f7de8dfe4afb7a9f3843d78c66
Rene Weteling <rene@weteling.com>
2017-08-15 07:54:37 +0200

    D

--- app/jobs/backup_job.rb
+++ app/jobs/backup_job.rb
@@ -30,5 +30,4 @@ class BackupJob < ApplicationJob
   def log(msg)
     `curl -X POST -H 'Content-type: application/json' --data '{"text":"#{msg}"}' https://hooks.slack.com/services/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`
   end
-  
 end
